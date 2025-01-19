b1a4abdad7feca776781961a7c2eb805c7af4247
aleksey <aleksey.bortnik91@gmail.com>
2025-01-19 21:31:32 +0300

    cards adjusted

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,40 +1,48 @@
 import React from "react";
 import "./card.scss";
-import { Video } from "src/types/video.type";
-import { useNavigate } from 'react-router-dom';
 
 interface CardProps {
-  video: Video;
-  isFeatured?: boolean;
+  video: {
+    snippet: {
+      title: string;
+      channelTitle: string;
+      thumbnails: { high: { url: string } };
+    };
+    statistics: {
+      viewCount: string;
+      likeCount: string;
+      dislikeCount: string;
+    };
+  };
 }
 
-export const Card: React.FC<CardProps> = ({ video, isFeatured }) => {
-  const { snippet, statistics } = video;
-    const navigate = useNavigate();
-  
-    // Переписать через делегирование
-    const handleNavigeToCardDetails = () => {
-      navigate('/card-info');
-    };
-
+export const Card: React.FC<CardProps> = ({ video }) => {
   return (
-    <div className={`card ${isFeatured ? "featured" : ""}`}>
-    
-      <img src={snippet.thumbnails.high.url} alt={snippet.title} />
-
-      <div className="card-content">
-        <h3>{snippet.title}</h3>
-        <p>{snippet.channelTitle}</p>
-
-        <div className="card-stats">
-          <span>{statistics.viewCount} просмотров</span>
-          <span>{statistics.likeCount} лайков</span>
-          <span>{statistics.dislikeCount} дизлайков</span>
-          <span>{statistics.commentCount} комментариев</span>
+    <div className="card">
+      <img
+        className="card__thumbnail"
+        src={video.snippet.thumbnails.high.url}
+        alt={video.snippet.title}
+      />
+      <div className="card__content">
+        <div className="card__title">{video.snippet.title}</div>
+        <div className="card__channel">{video.snippet.channelTitle}</div>
+        <div className="card__stats">
+          <div className="card__stat">
+            <img src="/assets/images/views.png" alt="views" />
+            {video.statistics.viewCount}
+          </div>
+          <div className="card__stat">
+            <img src="/assets/images/likes.png" alt="likes" />
+            {video.statistics.likeCount}
+          </div>
+          <div className="card__stat">
+            <img src="/assets/images/dislikes.png" alt="dislikes" />
+            {video.statistics.dislikeCount}
+          </div>
         </div>
-
-        <button className="card-next-btn" onClick={handleNavigeToCardDetails}>Далее</button>
       </div>
+      <div className="card__button">Подробнее</div>
     </div>
   );
 };
